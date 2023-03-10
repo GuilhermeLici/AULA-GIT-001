14325b7cbf167d4d698a170c7cddbdf064400eca
unknown <you@example.com>
2023-03-10 16:52:49 -0300

    adicionando imagem

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View ,Button} from 'react-native';
 
 export default function App() {
   return (
@@ -10,7 +10,7 @@ export default function App() {
       </Text>
       <Text>
         Open up App.js to start working on your app!
-
+      <button></button>
       </Text>
     </View>
   );
